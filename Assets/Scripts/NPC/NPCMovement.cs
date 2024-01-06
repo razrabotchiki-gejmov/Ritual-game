837e0f15bd479da873057149659b22f821417f0c
Azicel <Elenapor12@gmail.com>
2024-01-06 20:51:37 +0500

    Поворот стражников

--- Assets/Scripts/NPC/NPCMovement.cs
+++ Assets/Scripts/NPC/NPCMovement.cs
@@ -15,8 +15,6 @@ public class NPCMovement : MonoBehaviour
     // 0 - ничего
     // 1 - поесть
     public List<Vector2> spots;
-    public List<Quaternion> rotations;
-    private int rotIndex;
     private int spotIndex;
     public List<Sprite> sprites = new();
     public NPCState npcState;
@@ -27,8 +25,7 @@ public class NPCMovement : MonoBehaviour
     {
         body = GetComponentInChildren<NPCVision>().transform;
         spriteRenderer = GetComponent<SpriteRenderer>();
-        if(cooldowns.Count> 0 )
-            timeToMove = cooldowns[0];
+        timeToMove = cooldowns[0];
         npcState = GetComponent<NPCState>();
     }
 
@@ -48,26 +45,6 @@ public class NPCMovement : MonoBehaviour
             {
                 ChangeSpot();
             }
-        }
-        else if(npcState.type == 2)
-        {
-            if (rotations.Count > 0)
-            {
-                var rot = rotations[rotIndex].eulerAngles;
-                if(rot.z > body.rotation.z )
-                {
-                    body.Rotate(rot * Time.deltaTime * 0.1f);
-                }
-                else
-                {
-                    timeToMove -= Time.deltaTime;
-                }
-                
-                if (timeToMove <= 0)
-                {
-                    ChangeSpot();
-                }
-            }   
         }
     }
 
@@ -132,16 +109,8 @@ public class NPCMovement : MonoBehaviour
         {
             GetComponent<FoodEating>().EatFood();
         }
-        if (spots.Count > 0)
-        {
-            spotIndex = (spotIndex + 1) % spots.Count;
-            timeToMove = cooldowns[spotIndex];
-        }
-        else
-        {
-            rotIndex = (rotIndex + 1) % rotations.Count;
-            timeToMove = cooldowns[rotIndex];
-        }
+        spotIndex = (spotIndex + 1) % spots.Count;
+        timeToMove = cooldowns[spotIndex];
     }
 
 }
